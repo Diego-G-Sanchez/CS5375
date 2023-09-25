fa955540bd30be183d8cae7155199f00e5cbdbdd
Diego-G-Sanchez <dgsanchez4@miners.utep.edu>
2023-09-25 01:39:40 -0600

    Update createUsers.py

--- createUsers.py
+++ createUsers.py
@@ -3,9 +3,9 @@ import random
 import sys
 
 def create_user(username, password):
-  subprocess.run(["useradd", username])
-  proc = subprocess.Popen(['passwd', username], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-  stdout, stderr = proc.communicate(input=f"{password}\n{password}\n".encode())
+    subprocess.run(["useradd", username])
+    proc = subprocess.Popen(['passwd', username], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate(input=f"{password}\n{password}\n".encode())
 
 if len(sys.argv) != 2:
     print("Usage: file.py <passwords_file>")
@@ -14,10 +14,12 @@ if len(sys.argv) != 2:
 password_file = sys.argv[1]
 
 with open(password_file, 'r') as file:
-  lines = file.readlines()
+    lines = file.readlines()
 
-for i in range(30):
-  username = "user" + str(i)
-  password = random.choice(lines).strip()
-  print(password)
-  create_user(username, password)
+# Open a file to save the created usernames and their passwords
+with open("chosen_passwords.txt", "w") as out_file:
+    for i in range(30):
+        username = "user" + str(i)
+        password = random.choice(lines).strip()
+        create_user(username, password)
+        out_file.write(f"{password}\n")
